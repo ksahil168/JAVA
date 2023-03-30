235dcd9864bf114aba31e298a8f07a8914f3192c
Kumar Sahil <2270168@kiit.ac.in>
2023-03-31 00:01:58 +0530

    Display Array by recursion

---
+++ DSA with java/Recursion/display_array.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class display_array {
+
+    public static void display_array2(int[] arr, int i) {
+
+        if (i == arr.length) {
+            return;
+        }
+        System.out.println(arr[i]);
+        display_array2(arr, i + 1);
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 10, 20, 30, 40, 50 };
+        display_array2(arr, 0);
+
+    }
+}
